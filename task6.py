36b29653cdf0e17a1687fd5f89bab89b6960b695
artemwoka <artemwoka@gmail.com>
2026-01-17 14:52:40 +0100

    Add task6.py

---
+++ task6.py
@@ -0,0 +1,24 @@
+"""
+Помилки (номери рядків через пробіл, цей рядок - №2): 11 14 15
+"""
+
+
+def unemployment_rate(unemployed, employed):
+    """Повернути рівень безробіття (РБ) як частку від 1.
+
+       Розрахунок за формулою: РБ = безробітні / (зайняті + безробітні).
+    """
+    if employed + unemployed == 0:
+        raise ZeroDivisionError("Сума зайнятих і безробітних не може бути нулем.")
+    else:
+        return unemployed / (unemployed + employed)
+
+try:
+    unemployed = int(input("Введіть кількість безробітних (людей): "))
+    employed = int(input("Введіть кількість зайнятих (людей): "))
+    rate = unemployment_rate(unemployed, employed)
+    print(f"Рівень безробіття = {rate:.1%}")
+except ValueError:
+    print("Помилка: введено некоректне значення. Будь ласка, введіть число.")
+except Exception as e:
+    print(f"Сталася помилка: {e}")
